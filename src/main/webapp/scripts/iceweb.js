146e170c6bdf0cc4b7304bfab09d364fecbedc5f
Wang <Li.Wang2@unisys.com>
2018-06-06 13:41:50 -0400

    add details

--- src/main/webapp/scripts/iceweb.js
+++ src/main/webapp/scripts/iceweb.js
@@ -1,4 +1,5 @@
 var iceApp = angular.module('iceApp', []);
+var serviceURL = "http://192.60.241.81:8080";
 
 iceApp.controller('iceCtrl', function($scope, $http, $q, $location) {
 
@@ -6,7 +7,7 @@ iceApp.controller('iceCtrl', function($scope, $http, $q, $location) {
     //$scope.icePersonData = [{"id":97781502,"firstName":null,"lastName":null},{"id":97781504,"firstName":null,"lastName":null}];
 	$scope.init = function(){	
 		console.log("Angular HTTP init()....");
-		$http.get("http://liwang.unisys.com:8080/Persons")
+		$http.get(serviceURL + "/Persons")
 		.then(function success(response) {
 			//$scope.iceData = JSON.parse(response.data);
 			$scope.iceData = response.data;
@@ -18,9 +19,6 @@ iceApp.controller('iceCtrl', function($scope, $http, $q, $location) {
 
 	$scope.getPerson = function(){	
 		console.log("Angular HTTP getPerson....");
-
-        //$scope.id = $routeParams.id;
-        //var routeID = $routeParams.id;
 		
         var url = $location.absUrl().split("/").pop();
         var vars = {};
@@ -28,19 +26,28 @@ iceApp.controller('iceCtrl', function($scope, $http, $q, $location) {
 	        vars[key] = value;
 	    });	      
 
-		$http.get("http://liwang.unisys.com:8080/Person/" + vars["id"])
+		$http.get(serviceURL + "/Person/" + vars["id"])
 		.then(function success(response) {
 			//$scope.icePersonData = JSON.parse(response.data);
 			$scope.icePersonData = response.data;
 			console.log("AppData from Server: " + JSON.stringify(response.data));
 			
-				$http.get("http://liwang.unisys.com:8080/applications/" + vars["id"])
+				$http.get(serviceURL + "/applications/" + vars["id"])
 				.then(function success(response) {
-					//$scope.icePersonData = JSON.parse(response.data);
 					$scope.iceApplicationData = response.data;
-					console.log("AppData from Server: " + JSON.stringify(response.data));		
+					console.log("AppData from Server: " + JSON.stringify(response.data));
+					
+					$http.get(serviceURL + "/preenroll/" + vars["id"])
+					$http.get(serviceURL + "/preenroll/149132")
+					.then(function success(response) {
+						$scope.iceEnrollData = response.data;
+						console.log("AppData from Server: " + JSON.stringify(response.data));
+					}, function error(response) {
+			            $scope.icePersonData = response.status + " " + response.statusText + " " + response.headers();
+			        });	
+		        												
 				}, function error(response) {
-		            $scope.iceApplicationData = response.status + " " + response.statusText + " " + response.headers();
+		            $scope.icePersonData = response.status + " " + response.statusText + " " + response.headers();
 		        });			
 					
 		}, function error(response) {
